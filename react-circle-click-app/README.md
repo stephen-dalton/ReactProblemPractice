cd287cb5ea09ea5be40667105c105284a7c7dd4d
Stephen Dalton <daltsdev@icloud.com>
2022-11-15 21:20:27 +0000

    React Point Problem

---
+++ react-circle-click-app/README.md
@@ -0,0 +1,16 @@
+### React Point Problem
+
+---
+
+#### Problem Statement
+
+1. Create an app where you can click anywhere on the page, and it produces a cirle in the position the mouse clicks.
+2. Implement an undo functionality, where the last point added is removed from the page.
+3. Implement a redo functionality, where the last point removed from the page is re-added.
+4. Implement a reset functionality, where all points are removed from the page.
+
+##### Solution
+
+Solution Can be found in `./src/App.jsx`
+
+To run the solution, clone the repo, run npm install & npm run dev.
---
+++ react-circle-click-app/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [points, setPoints] = useState([]);
+  const [pointsStack, setPointsStack] = useState([]);
+
+  const handlePointCreate = (e) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, { x: clientX - 10, y: clientY - 32 }]);
+  };
+
+  const handlePointRedo = () => {
+    // create a copy of the current points stack
+    const pointsStackCopy = [...pointsStack];
+    // get the last point pushed to the stack, and remove it from stack;
+    const lastPointAdded = pointsStackCopy.pop();
+    // if no more elements in the stack, return;
+    if (!lastPointAdded) return;
+    // update points to include previous points and the last element added to the stack
+    setPoints([...points, lastPointAdded]);
+    // update stack to a new stack without the last element added
+    setPointsStack(pointsStackCopy);
+  };
+
+  const handlePointUndo = () => {
+    // Get a copy of points
+    const pointsCopy = [...points];
+    // get the last point, and pop off copy of points
+    const lastPointAdded = pointsCopy.pop();
+    // if there is no elements left in the points array return
+    if (!lastPointAdded) return;
+    // Update the stack with the previous stack, and most recent point created
+    setPointsStack([...pointsStack, lastPointAdded]);
+    // set the points state to not include previously added point
+    setPoints(pointsCopy);
+  };
+
+  const handleReset = () => {
+    setPoints([]);
+    setPointsStack([]);
+  };
+  return (
+    <>
+      <div className="buttons">
+        <button onClick={handlePointRedo}>REDO</button>
+        <button onClick={handlePointUndo}>UNDO</button>
+        <button onClick={handleReset}>RESET</button>
+      </div>
+      <div className="container" onClick={handlePointCreate}>
+        {points.map((point, i) => (
+          <div
+            key={i}
+            className="point"
+            style={{ top: point.y, left: point.x }}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
